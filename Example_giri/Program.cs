b155b3f2fe098ab55a403e0cf42f8a1b79cb98bd
Ivan <nindsia146@mail.ru>
2022-11-29 17:15:44 +0400

    Нахождение самого большого

---
+++ Example_giri/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 1;
+int b = 6;
+int d = 8;
+int c = 2;
+int i = 3;
+
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(d > max) max = d;
+if(c > max) max = c;
+if(i > max) max = i;
+
+Console.Write("max= ");
+Console.WriteLine(max);
