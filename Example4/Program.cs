ced7e26a99ad2b490a4955d73d409708523e19b2
Ivan <nindsia146@mail.ru>
2022-11-29 17:09:31 +0400

    Нижний регистор ToLower()

---
+++ Example4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string name = Console.ReadLine();
+
+if(name.ToLower() == "иван")
+    {
+    Console.WriteLine("Ура, Иван");
+} 
+else
+{
+    Console.Write("Привет ");
+    Console.WriteLine(name);
+}
